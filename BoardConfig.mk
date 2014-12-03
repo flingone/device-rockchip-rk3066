9203495c027129540d6bd2abef20980259e631ee
rochy2014 <rochy.rong@gmail.com>
2014-12-03 13:04:12 +0800

    BoardConfig.mk:set BOARD_USE_LOW_MEM to false for supporting 1G RAM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,7 +8,7 @@ TARGET_BOARD_PLATFORM := rk3066
 TARGET_BOARD_PLATFORM_GPU := mali400
 
 #if board only 512M ddr, set true, esle false
-BOARD_USE_LOW_MEM := true
+BOARD_USE_LOW_MEM := false
 
 # google apps
 BUILD_WITH_GOOGLE_MARKET ?= false
